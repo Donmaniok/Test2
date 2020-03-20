49a2dfb6049be949d70c37f75aad7842c6dc5a53
Donmaniok <grudzinski-p@o2.pl>
2020-03-20 23:26:07 +0100

    Projekty IT   v1.0

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # README
 
+_**20-03-2020**_  
+_**Test v1.o**_
+
 # Spis treści:
+
+
+- **1. Zmienne `int` oraz `char`**
+- **2. Typy danych**
+- **3. Pętla For**
+> klawisz 0 wyjście z programu  
+>
+*__________________*
+
+
+<code>(auto numeracja)</code>
 <ol>
 <li>info</li>
 <li>instr</li>
@@ -9,13 +23,18 @@
 <li>= wynik</li>
 </ol>
 
+Informacje:
+===========
+
+
+# *Komentarze:*
+
+>
+* > Wcięcie :D
+> Last Line
 
 
 
-
-
-
-_**C++**_
 
 
 ## Author
--- main.cpp
+++ main.cpp
@@ -3,77 +3,93 @@
 //
 #include <iostream>
 #include <cstdlib>
-
-
-///             ?????????????????????
-/// #define EXIT    main(goto koniec);
-///
-
-
+#include <zconf.h>          //  sleep()
+#include <cstdio>
+#include <curses.h>
 
 using namespace std;
 
-void menuu();
 void menu_1();
-
+void menu_2();
+void menu_3();
 
 int main() {
-
+/**---------------------------          0. Start    ----------------------------------------**/
     int menu;
 
-
     cout << "Standordowo:\t\t\t Hello, World!" << endl << "\t\tCzesc!" << endl
-<< "\"Wpisz\" (cyfre) gdzie chcesz przejsc: " << endl << endl << "\t\t";      cin >> menu;
-
-//  1,2,3,...|
-
-    cout << "\n\t\tDo zobaczenia!";
+<< "\"Wpisz\" (cyfre) gdzie chcesz przejsc: " << endl << endl;
 
 
-    return 0;
-}
+cout << endl << "\t1. Liczba i napis\n"
+        "\t2. Wyswietl typy danych\n"
+        "\t3. Wczytaj n zmiennych"
+        "\t 0 Wyjscie z programu\n"<< endl;
+cout << "\t\t";
+    More:
+cin >> menu;
 
-/*
-void menu() {
-            int nr_menu = atexit();
-//char quit;
-//    for(; ;){
-//        quit = getchar();
-//        if (quit == '')
-//    };
 
-                    if (nr_menu != 113){
-                        return (0);
-                    }
-
-    cout << "Wpisz nr." << endl;
-    cout << "\t1. AaA \t2. BbB \t3. CcC" << endl;
-    cout << "\t\tNacisnij 0, aby wyjsc z programu." << endl;    ///dodac do VOIDA !!!!
-Try:
-    cin >> nr_menu;
-
-    switch (nr_menu){
+    switch (menu){
+        case 0:
+            break;
         case 1:
-            cout << "numer jeden 1.";
             menu_1();
             break;
         case 2:
-            cout << "dwa = yyyyy";
+            menu_2();
             break;
         case 3:
-            cout << "TRZY" << endl;
-            break;
-
-        case 0:
-            cout << endl; system ("pause");
+            menu_3();
             break;
         default:
-            cout << "Wprowadzona wartosc: " << nr_menu << " jest nieprawidlowa.\n Sprobuj jeszcze raz!\n\n";
-            goto Try;
+            cout << "\n\n\tWprowadz CYFRE\n";
+            goto More;
     }
+
+    cout << "\n\t\tDo zobaczenia!";
+    return 0;
 }
 
 void menu_1(){
-    cout <<"\n\n\t\taaaaaaaaaaaa\n";
+    char imie;
+    int liczba;
+    cout <<"\n\n\t\tWpisz Imie: ";
+    cin >> imie;
+    cout << "\tOK \nWprowadz cyfre: ";
+    cin >> liczba;
+    cout << "\n\t\tWprowadzono:\n \tImie: " << imie << "\n\tCyfre: " << liczba;
 }
- */             /// ..To Do !
+
+void menu_2(){
+    cout << "\n\t\t\tTYPY DANYCH:: " << endl << endl;
+    cout << "\n\tInt \t --> \tliczby całkowite"
+            "\n\tFloat \t --> \tliczby zmiennoprzecinkowe"
+            "\n\tDouble \t --> \tliczby zmiennoprzecinkowe"
+            "\n\tBoolean\t --> \tprawda / fałsz"
+            "\n\tChar\t --> \tjeden znak"
+            "\n\tString\t --> \tciąg znaków"
+            "\n\nKoniec.";
+}
+
+void menu_3(){
+
+    int ilosc;
+    int liczba_1, liczba_n;
+
+    cout << "Ile liczb chcesz wczytac ?" << endl;
+    cin >> ilosc;
+
+    cout << "Wpisz " << ilosc << " liczb." << endl;
+    cin >> liczba_1;
+    cout << "liczba: " << liczba_1 << endl;
+
+    for (int i = 1; i < ilosc; i++) {
+        cin >> liczba_n;
+        cout << "liczba: " << liczba_n << endl;
+        if (liczba_1 < liczba_n){
+            liczba_1 = liczba_n;
+        }
+    }
+    cout << "podales: " << ilosc << " liczb.\tNajwieksza z nich to:\t" << liczba_1 << endl;
+}
